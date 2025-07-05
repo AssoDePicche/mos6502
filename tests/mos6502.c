da47a95d1348e5bbc6eb4a4f527c84360e6af81c
AssoDePicche <samuelprado730@gmail.com>
2025-07-04 23:10:45 -0300

    test: update to mos6502 api

--- tests/mos6502.c
+++ tests/mos6502.c
@@ -2,74 +2,32 @@
 
 #include <unity.h>
 
+typedef void (*test_t)(void);
+
 static MOS6502 *CPU = NULL;
 
 void setUp(void) { CPU = mos6502_construct(); }
 
 void tearDown(void) { mos6502_destruct(CPU); }
 
-void LDA_Immediate_Can_Load_A_Value_Into_Register_A(void) {
-  mos6502_write(CPU, 0xFFFC, MOS6502_LDA_IMMEDIATE_MODE);
-
-  mos6502_write(CPU, 0xFFFD, 0x84);
-
-  CPU->current_instruction_cycles = 2;
-
-  mos6502_execute(CPU);
-
-  TEST_ASSERT_EQUAL_HEX8(CPU->A, 0x84);
-}
-
-void LDA_Zero_Page_Can_Load_A_Value_Into_Register_A(void) {
-  mos6502_write(CPU, 0xFFFC, MOS6502_LDA_ZERO_PAGE_MODE);
-
-  mos6502_write(CPU, 0xFFFD, 0x42);
-
-  mos6502_write(CPU, 0x0042, 0x37);
-
-  CPU->current_instruction_cycles = 3;
-
-  TEST_ASSERT_EQUAL_HEX8(CPU->A, 0x37);
-}
-
-void LDA_Zero_Page_X_Can_Load_A_Value_Into_Register_A(void) {
-  CPU->X = 5;
-
-  mos6502_write(CPU, 0xFFFC, MOS6502_LDA_ZERO_PAGE_X_MODE);
-
-  mos6502_write(CPU, 0xFFFD, 0x42);
-
-  mos6502_write(CPU, 0x0047, 0x37);
-
-  CPU->current_instruction_cycles = 4;
-
-  TEST_ASSERT_EQUAL_HEX8(CPU->A, 0x37);
-}
-
-void LDA_Zero_Page_X_Can_Load_A_Value_Into_Register_A_When_It_Wraps(void) {
-  CPU->X = 0xFF;
-
-  mos6502_write(CPU, 0xFFFC, MOS6502_LDA_ZERO_PAGE_X_MODE);
-
-  mos6502_write(CPU, 0xFFFD, 0x80);
-
-  mos6502_write(CPU, 0x007F, 0x37);
-
-  CPU->current_instruction_cycles = 4;
-
-  TEST_ASSERT_EQUAL_HEX8(CPU->A, 0x37);
-}
+static const test_t TESTS[] = {
+    NULL,
+};
 
 int main(void) {
   UNITY_BEGIN();
 
-  RUN_TEST(LDA_Immediate_Can_Load_A_Value_Into_Register_A);
+  for (size_t index = 0; index < sizeof(TESTS) / sizeof(test_t); ++index) {
+    const test_t test = TESTS[index];
 
-  RUN_TEST(LDA_Zero_Page_Can_Load_A_Value_Into_Register_A);
+    if (NULL == test) {
+      fprintf(stderr, "Error: could not run test");
 
-  RUN_TEST(LDA_Zero_Page_X_Can_Load_A_Value_Into_Register_A);
+      break;
+    }
 
-  RUN_TEST(LDA_Zero_Page_X_Can_Load_A_Value_Into_Register_A_When_It_Wraps);
+    RUN_TEST(test);
+  }
 
   return UNITY_END();
 }
